e28c0d830600230fc00eb2085e0c3997ecb7bbdd
darthvader1925 <elijb12@gmail.com>
2023-12-10 20:35:21 -0800

    Stuff

--- src/scanner.js
+++ src/scanner.js
@@ -1,4 +1,23 @@
 /**
  * @name scanner
  * @description scans files for viruses
- *  */
+ */
+
+const NodeClam = require('clamscan');
+const ClamScan = new NodeClam().init({
+    debugMode: true,
+    removeInfectedFiles: false,
+    quarantineInfectedFiles: false, // path if true, but custom quarantine function
+    clamscan: {
+        scanArchives: true
+    }
+});
+
+ClamScan.then(async clamscan => {
+
+module.exports = {
+    scamClan: async function (path) {
+            return await clamscan.isInfected(path)
+        }
+    }
+})
